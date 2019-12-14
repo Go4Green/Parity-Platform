2cfced24b9382fa5e666df619e272618bde5af19
Alexios Karadimos <karadalex@gmail.com>
2019-12-14 11:40:38 +0200

    setup vuetify js framework

--- csms/webapp/resources/js/app.js
+++ csms/webapp/resources/js/app.js
@@ -3,7 +3,7 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
+import vuetify from './plugins/vuetify' // path to vuetify export
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -18,8 +18,7 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('app', require('./components/App.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +28,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    vuetify
 });
---
+++ csms/webapp/resources/js/plugins/vuetify.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+    breakpoint: {
+        // Breakpoints
+        xs: true,
+        sm: true,
+        md: true,
+        lg: true,
+        xl: true,
+
+        // Conditionals
+        xsOnly: true,
+        smOnly: true,
+        smAndDown: true,
+        smAndUp: true,
+        mdOnly: true,
+        mdAndDown: true,
+        mdAndUp: true,
+        lgOnly: true,
+        lgAndDown: true,
+        lgAndUp: true,
+        xlOnly: true,
+
+        // Current breakpoint name (e.g. 'md')
+        // name: string
+
+        // Dimensions
+        // height: number
+        // width: number,
+
+        thresholds: {
+          xs: 340,
+          sm: 540,
+          md: 800,
+          lg: 960,
+          xl: 1280,
+        },
+        scrollBarWidth: 24,
+    },
+
+    theme: {
+        dark: true,
+    },
+})
+
