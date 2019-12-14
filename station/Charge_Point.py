b1a788e7dab64d5882f5afe11327899a7a0914e7
nickarad <nickarad94@gmail.com>
2019-12-14 10:21:29 +0000

    Initial Commit

---
+++ station/Charge_Point.py
@@ -0,0 +1,38 @@
+import asyncio
+import websockets
+
+from ocpp.v20 import call
+from ocpp.v20 import ChargePoint as cp
+
+
+class ChargePoint(cp):
+
+    async def send_boot_notification(self):
+        request = call.BootNotificationPayload(
+                charging_station={
+                    'model': 'Wallbox XYZ',
+                    'vendor_name': 'anewone'
+                },
+                reason="PowerUp"
+        )
+        response = await self.call(request)
+
+        if response.status == 'Accepted':
+            print("Connected to central system.")
+            print(request)
+            print(response)
+
+
+async def main():
+    async with websockets.connect(
+        'ws://localhost:9000/CP_1',
+         subprotocols=['ocpp2.0']
+    ) as ws:
+
+        cp = ChargePoint('CP_1', ws)
+
+        await asyncio.gather(cp.start(), cp.send_boot_notification())
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ station/README.md
@@ -0,0 +1,22 @@
+Installing packages for your project
+>$ pip install -r requirements.txt
+
+Create new virtual environment
+>$  virtualenv venv
+
+
+To begin using the virtual environment, it needs to be activated:
+
+>$ source venv/bin/activate
+
+
+If you are done working in the virtual environment for the moment, you can deactivate it:
+
+>$ deactivate
+
+Documentation
+https://ocpp.readthedocs.io/en/latest/index.html
+
+
+
+
