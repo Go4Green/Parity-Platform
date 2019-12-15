b1280bef1f2eb77c543a41191f1e409f9581a7f5
nickarad <nickarad94@gmail.com>
2019-12-15 09:56:49 +0000

    Remove useless code

--- station/readmifare.py
+++ station/readmifare.py
@@ -21,18 +21,6 @@ def read_nfc():
         if uid is None:
             continue
         print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
-        # Authenticate block 4 for reading with default key (0xFFFFFFFFFFFF).
-        if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
-                                                    [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
-            print('Failed to authenticate block 4!')
-            continue
-        # Read block 4 data.
-        data = pn532.mifare_classic_read_block(4)
-        if data is None:
-            print('Failed to read block 4!')
-            continue
-        # Note that 16 bytes are returned, so only show the first 4 bytes for the block.
-        print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
         flag = False
         
 if __name__ == '__main__':
